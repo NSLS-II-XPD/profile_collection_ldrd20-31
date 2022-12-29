28cadf01980ef50e0634dbf985409e7cb16478dd
XPD Operator <xf28id2@lob1-xpd-ws1.nsls2.bnl.local>
2022-12-29 17:17:38 -0500

    Add metadata info

--- bundle-plan/32-bundle-plan.py
+++ bundle-plan/32-bundle-plan.py
@@ -3,10 +3,25 @@ from bluesky.utils import ts_msg_hook
 RE.msg_hook = ts_msg_hook
 
 
-def outer_plan(det1, det2, *args, md={}, num_fast=10, **kwargs):
+def outer_plan(det1, det2, *args, md=None, num_fast=10, sample_type = 'test', 
+               spectrum_type='Absorbtion', correction_type='Reference', 
+               pump_list=None, precursor_list=None, mixer=None, note=None, **kwargs):
+    
+    if (pump_list != None and precursor_list != None):
+        _md = {"pumps" : [pump.name for pump in pump_list], 
+               "precursors" : precursor_list, 
+               "infuse_rate" : [pump.read_infuse_rate.get() for pump in pump_list], 
+               "infuse_rate_unit" : [pump.read_infuse_rate_unit.get() for pump in pump_list],
+               "pump_status" : [pump.status.get() for pump in pump_list], 
+               "uvvis" :[spectrum_type, correction_type, qepro.integration_time.get(), qepro.num_spectra.get()], 
+               "mixer": mixer,
+               "sample_type": sample_type,
+               "note" : note if note else "None"}
+        _md.update(md or {})    
+    
     @bpp.stage_decorator([det1, det2])
-    @bpp.run_decorator(md=md)
-    def trigger_two_detectors():  # TODO: rename appropriately
+    @bpp.run_decorator(md=_md)
+    def trigger_two_detectors():  # TODO: rename appropriately        
         yield from bps.trigger(det1)
 
         ret = {}
@@ -16,7 +31,7 @@ def outer_plan(det1, det2, *args, md={}, num_fast=10, **kwargs):
         for i in range(num_fast):
             yield from bps.trigger(det2)
 
-            yield from bps.create(name="absorbance")
+            yield from bps.create(name="xray")
             reading = (yield from bps.read(det2))
             print(f"reading = {reading}")
             ret.update(reading)
@@ -25,7 +40,7 @@ def outer_plan(det1, det2, *args, md={}, num_fast=10, **kwargs):
         for i in range(num_fast * 2):  # TODO: fix the number of triggers
             yield from bps.trigger(det2)
 
-            yield from bps.create(name="fluorescence")
+            yield from bps.create(name="uvvis")
             reading = (yield from bps.read(det2))
             print(f"reading = {reading}")
             ret.update(reading)
@@ -41,5 +56,5 @@ def outer_plan(det1, det2, *args, md={}, num_fast=10, **kwargs):
         print(f"reading = {reading}")
         ret.update(reading)
         yield from bps.save()
-    
+
     yield from trigger_two_detectors()
