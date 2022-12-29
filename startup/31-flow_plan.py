6c1ed1f0e0bd361788e913c3b61f37ab6b271a41
XPD Operator <xf28id2@lob1-xpd-ws1.nsls2.bnl.local>
2022-12-29 17:19:00 -0500

    Add syringe material into plan

--- startup/31-flow_plan.py
+++ startup/31-flow_plan.py
@@ -1,3 +1,13 @@
+'''
+pump_list = [dds1_p1]
+syringe_list = [2.5]
+target_vol_list = ['0.5 ml']
+infuse_rates = ['500 ul/min']
+precursor_list = ['test']
+mixer = [None]
+syringe_mater_list=['glass_H1000']
+'''
+
 def reset_pumps(pump_list, clear=True, update = '.2 second'):
     for pump in pump_list:
         pump.initialize_pump(clear=clear, update = update)
@@ -28,21 +38,25 @@ def show_pump_status(syringe_list, pump_list, precursor_list, syringe_mater_list
         print('\n')
         
 
-def set_group_infuse(syringe_list, pump_list, target_vol_list=['50 ml', '50 ml'], rate_list = ['100 ul/min', '100 ul/min']):
-    for i, j, k, l in zip(pump_list, target_vol_list, rate_list, syringe_list):
+def set_group_infuse(syringe_list, pump_list, target_vol_list=['50 ml', '50 ml'], 
+                     rate_list = ['100 ul/min', '100 ul/min'], syringe_mater_list=['steel', 'steel']):
+    for i, j, k, l, m in zip(pump_list, target_vol_list, rate_list, syringe_list, syringe_mater_list):
         vol = float(j.split(' ')[0])
         vol_unit = j.split(' ')[1]
         rate = float(k.split(' ')[0])
         rate_unit = k.split(' ')[1]        
-        yield from i.set_infuse(l, target_vol = vol, target_unit = vol_unit, infuse_rate = rate, infuse_unit = rate_unit)
+        yield from i.set_infuse(l, target_vol = vol, target_unit = vol_unit, infuse_rate = rate, 
+                                infuse_unit = rate_unit, syringe_material=m)
         
-def set_group_withdraw(syringe_list, pump_list, target_vol_list=['50 ml', '50 ml'], rate_list = ['100 ul/min', '100 ul/min']):
-    for i, j, k, l in zip(pump_list, target_vol_list, rate_list, syringe_list):
+def set_group_withdraw(syringe_list, pump_list, target_vol_list=['50 ml', '50 ml'], 
+                       rate_list = ['100 ul/min', '100 ul/min'], syringe_mater_list=['steel', 'steel']):
+    for i, j, k, l, m in zip(pump_list, target_vol_list, rate_list, syringe_list, syringe_mater_list):
         vol = float(j.split(' ')[0])
         vol_unit = j.split(' ')[1]
         rate = float(k.split(' ')[0])
         rate_unit = k.split(' ')[1]        
-        yield from i.set_withdraw(l, target_vol = vol, target_unit = vol_unit, withdraw_rate = rate, withdraw_unit = rate_unit)
+        yield from i.set_withdraw(l, target_vol = vol, target_unit = vol_unit, withdraw_rate = rate, 
+                                  withdraw_unit = rate_unit, syringe_material=m)
 
 
 def start_group_infuse(pump_list):
