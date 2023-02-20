f44925b41e40049c07988b897d87cf6496aa871b
XPD Operator <xf28id2@lob1-xpd-ws1.nsls2.bnl.local>
2023-02-20 17:35:59 -0500

    Update plan for qepro collection

--- bundle-plan/32-bundle-plan.py
+++ bundle-plan/32-bundle-plan.py
@@ -3,7 +3,7 @@ from bluesky.utils import ts_msg_hook
 RE.msg_hook = ts_msg_hook
 
 
-def outer_plan(det1, det2, *args, md=None, num_fast=10, sample_type = 'test', 
+def xray_uvvis_plan(det1, det2, *args, md=None, num_fast=10, sample_type = 'test', 
                spectrum_type='Absorbtion', correction_type='Reference', 
                pump_list=None, precursor_list=None, mixer=None, note=None, **kwargs):
     
@@ -18,7 +18,15 @@ def outer_plan(det1, det2, *args, md=None, num_fast=10, sample_type = 'test',
                "sample_type": sample_type,
                "note" : note if note else "None"}
         _md.update(md or {})    
-    
+        
+    if (pump_list == None and precursor_list == None):
+        _md = { "uvvis" :[spectrum_type, correction_type, qepro.integration_time.get(), qepro.num_spectra.get()], 
+                "mixer": 'exsitu measurement',
+                "sample_type": sample_type,
+                "note" : note if note else "None"}
+        _md.update(md or {})
+
+
     @bpp.stage_decorator([det1, det2])
     @bpp.run_decorator(md=_md)
     def trigger_two_detectors():  # TODO: rename appropriately        
@@ -27,22 +35,53 @@ def outer_plan(det1, det2, *args, md=None, num_fast=10, sample_type = 'test',
         ret = {}
 
         # TODO: write your fast procedure here, don't use bp.count/bp.scan here as they open separate runs.
-        #   Use `trigger_and_read` instead.
+        # Use `trigger_and_read` instead.
+        # Tested on 2023/02/16: bps.trigger works for qepro
+        
+        
+        # For absorbance: spectrum_type='Absorbtion', correction_type='Reference'
+        # For fluorescence: spectrum_type='Corrected Sample', correction_type='Dark'
+        
+        ## Start to collecting absrobtion
+        spectrum_type='Absorbtion'
+        correction_type='Reference'
+        if LED.get()=='Low' and UV_shutter.get()=='High' and qepro.correction.get()==correction_type and qepro.spectrum_type.get()==spectrum_type:
+            pass
+        else:
+            # yield from bps.abs_set(qepro.correction, correction_type, wait=True)
+            # yield from bps.abs_set(qepro.spectrum_type, spectrum_type, wait=True)
+            yield from bps.mv(qepro.correction, correction_type, qepro.spectrum_type, spectrum_type)
+            yield from bps.mv(LED, 'Low', UV_shutter, 'High')
+            yield from bps.sleep(1)
+
         for i in range(num_fast):
-            yield from bps.trigger(det2)
+            yield from bps.trigger(det2, wait=True)
 
             yield from bps.create(name="absorbance")
             reading = (yield from bps.read(det2))
-            print(f"reading = {reading}")
+            # print(f"reading = {reading}")
             ret.update(reading)
             yield from bps.save()  # TODO: check if it's needed, most likely yes.
 
+        
+        ## Start to collecting fluorescence
+        spectrum_type='Corrected Sample'
+        correction_type='Dark'
+        if LED.get()=='High' and UV_shutter.get()=='Low' and qepro.correction.get()==correction_type and qepro.spectrum_type.get()==spectrum_type:
+            pass
+        else:
+            # yield from bps.abs_set(qepro.correction, correction_type, wait=True)
+            # yield from bps.abs_set(qepro.spectrum_type, spectrum_type, wait=True)
+            yield from bps.mv(qepro.correction, correction_type, qepro.spectrum_type, spectrum_type)
+            yield from bps.mv(LED, 'High', UV_shutter, 'Low')
+            yield from bps.sleep(1)
+
         for i in range(num_fast * 2):  # TODO: fix the number of triggers
-            yield from bps.trigger(det2)
+            yield from bps.trigger(det2, wait=True)
 
             yield from bps.create(name="fluorescence")
             reading = (yield from bps.read(det2))
-            print(f"reading = {reading}")
+            # print(f"reading = {reading}")
             ret.update(reading)
             yield from bps.save()  # TODO: check if it's needed, most likely yes.
 
