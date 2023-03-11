dab691d963479e0e2070d20d2cbeb2688bcd34d8
XPD Operator <xf28id2@lob1-xpd-ws1.nsls2.bnl.local>
2023-03-11 18:24:16 -0500

    Add mother class of open_figures

--- scripts/plot_helper.py
+++ scripts/plot_helper.py
@@ -1,54 +1,61 @@
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy
-import matplotlib.gridspec as gridspec
 from side_functions import _readable_time
 
-class plot_uvvis():
+
+class open_figures():
+    def __init__(self, figure_labels):
+        for i in figure_labels:
+            plt.figure(num=i)
+
+
+class plot_uvvis(open_figures):
     
-    def __init__(self, uid, stream_name, qepro_dic, metadata_dic, fig = None):
-        self.fig = fig
+    def __init__(self, qepro_dic, metadata_dic, 
+                 figure_labels = ['primary_absorbance', 'primary_fluorescence', 
+                                  'bundle_absorbance', 'bundle_fluorescence']):
+        self.fig = figure_labels
         self.uid = metadata_dic['uid']
-        self.stream_name = stream_name
+        self.stream_name = metadata_dic['stream_name']
         self.qepro_dic = qepro_dic
         self.metadata_dic = metadata_dic
         self.wavelength = []
         self.output = []   
         # self.num = None
         self.date, self.time = _readable_time(metadata_dic['time'])
+        super().__init__(figure_labels)
 
 
     def plot_data(self):
         self.wavelength = self.qepro_dic['QEPro_x_axis']
+        self.output = self.qepro_dic['QEPro_output']
         
         if self.stream_name == 'absorbance':
-            self.output = self.qepro_dic['QEPro_output']
             y_label = 'Absorbance'
+            f = plt.figure(self.fig[2])
+            ax = f.gca()
         
         elif self.stream_name == 'fluorescence':
-            self.output = self.qepro_dic['QEPro_output']
             y_label = 'Fluorescence'
+            f = plt.figure(self.fig[3])
+            ax = f.gca()
         
         elif self.stream_name == 'primary':
             if self.qepro_dic['QEPro_spectrum_type'] == 3:
-                self.output = self.qepro_dic['QEPro_output']
                 y_label = 'Absorbance'
+                f = plt.figure(self.fig[0])
+                ax = f.gca()
             else:
-                self.output = self.qepro_dic['QEPro_output']
                 y_label = 'Fluorescence'
+                f = plt.figure(self.fig[1])
+                ax = f.gca()
 
-        
-        if self.fig is not None:
-            plt.close(self.fig.number)
-        self.fig = plt.figure()
-        ax = self.fig.gca()
-        # ax.scatter(xaxis, data, color='blue')
         for i in range(self.wavelength.shape[0]):
-            ax.plot(self.wavelength[i], self.output[i], label=f'{i:03d}')
+            ax.plot(self.wavelength[i], self.output[i], label=f'{self.time}_{i:03d}')
         # ax.set_facecolor((0.95, 0.95, 0.95))
         ax.set_xlabel('Wavelength (nm)', fontdict={'size': 14})
         ax.set_ylabel(y_label, fontdict={'size': 14})
         ax.set_title(f'{self.date}-{self.time}_{self.uid[0:8]}_{self.stream_name}')
         ax.legend()
-        self.fig.canvas.manager.show()
-        self.fig.canvas.flush_events()
+        f.canvas.manager.show()
+        f.canvas.flush_events()
