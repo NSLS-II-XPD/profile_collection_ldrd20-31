d59ed7f49b3ecd5bf16e0e000687b9884afcd865
XPD Operator <xf28id2@lob1-xpd-ws1.nsls2.bnl.local>
2023-03-11 18:28:34 -0500

    plot interactive figures w/ kafka.consume

---
+++ scripts/print_kafka_with_main4.py
@@ -0,0 +1,119 @@
+import datetime
+import pprint
+import uuid
+# from bluesky_kafka import RemoteDispatcher
+from bluesky_kafka.consume import BasicConsumer
+import matplotlib.pyplot as plt
+import time
+import databroker
+from side_functions import read_qepro_by_stream, dic_to_csv_for_stream
+import os
+from plot_helper import plot_uvvis
+# db = databroker.Broker.named('xpd-ldrd20-31')
+# catalog = databroker.catalog['xpd-ldrd20-31']
+
+try:
+    from nslsii import _read_bluesky_kafka_config_file  # nslsii <0.7.0
+except (ImportError, AttributeError):
+    from nslsii.kafka_utils import _read_bluesky_kafka_config_file  # nslsii >=0.7.0
+
+# these two lines allow a stale plot to remain interactive and prevent
+# the current plot from stealing focus.  thanks to Tom:
+# https://nsls2.slack.com/archives/C02D9V72QH1/p1674589090772499
+plt.ion()
+plt.rcParams["figure.raise_window"] = False
+
+
+def print_kafka_messages(beamline_acronym, csv_path):
+    print(f"Listening for Kafka messages for {beamline_acronym}")
+
+    global db, catalog
+    db = databroker.Broker.named(beamline_acronym)
+    catalog = databroker.catalog[f'{beamline_acronym}']
+
+    # plt.figure()
+    # def print_message(name, doc):
+    def print_message(consumer, doctype, doc):
+        name, message = doc
+        # print(
+        #     f"{datetime.datetime.now().isoformat()} document: {name}\n"
+        #     f"document keys: {list(message.keys())}\n"
+        #     f"contents: {pprint.pformat(message)}\n"
+        # )
+        if name == 'start':
+            print(
+                f"{datetime.datetime.now().isoformat()} documents {name}\n"
+                f"document keys: {list(message.keys())}")
+                
+            if 'uid' in message.keys():
+                print(f"uid: {message['uid']}")
+            if 'plan_name' in message.keys():
+                print(f"plan name: {message['plan_name']}")
+            if 'detectors' in message.keys(): 
+                print(f"detectors: {message['detectors']}")
+            if 'pumps' in message.keys(): 
+                print(f"pumps: {message['pumps']}")
+            # if 'detectors' in message.keys(): 
+            #     print(f"detectors: {message['detectors']}")
+            if 'uvvis' in message.keys() and message['plan_name']!='count':
+                print(f"uvvis mode:\n"
+                      f"           integration time: {message['uvvis'][0]} ms\n"
+                      f"           num spectra averaged: {message['uvvis'][1]}\n"
+                      f"           buffer capacity: {message['uvvis'][2]}"
+                      )
+            elif 'uvvis' in message.keys() and message['plan_name']=='count':
+                print(f"uvvis mode:\n"
+                      f"           spectrum type: {message['uvvis'][0]}\n"
+                      f"           integration time: {message['uvvis'][2]} ms\n"
+                      f"           num spectra averaged: {message['uvvis'][3]}\n"
+                      f"           buffer capacity: {message['uvvis'][4]}"
+                      )                
+            if 'mixer' in message.keys():
+                print(f"mixer: {message['mixer']}")
+            if 'sample_type' in message.keys():
+                print(f"sample type: {message['sample_type']}")
+            
+        if name == 'stop':
+            # print('Kafka test good!!')
+            print(f"{datetime.datetime.now().isoformat()} documents {name}\n"
+                  f"contents: {pprint.pformat(message)}"
+            )
+            # num_events = len(message['num_events'])
+
+            time.sleep(2)
+            uid = message['run_start']
+            print(f'\n*** start to export uid: {uid} ***')
+            for stream_name in ['primary', 'absorbance', 'fluorescence']:
+                if stream_name in message['num_events'].keys():
+                    qepro_dic, metadata_dic = read_qepro_by_stream(uid, stream_name=stream_name, data_agent='catalog')
+                    dic_to_csv_for_stream(csv_path, qepro_dic, metadata_dic, stream_name=stream_name)
+                    print(f'export {stream_name} in uid: {uid[0:8]} to ../{os.path.basename(csv_path)} done!')
+                    u = plot_uvvis(uid, stream_name, qepro_dic, metadata_dic)
+                    u.plot_data()
+            print('*** export complete ***\n')
+            print('########### Events printing division ############\n')
+
+    kafka_config = _read_bluesky_kafka_config_file(config_file_path="/etc/bluesky/kafka.yml")
+
+    # this consumer should not be in a group with other consumers
+    #   so generate a unique consumer group id for it
+    unique_group_id = f"echo-{beamline_acronym}-{str(uuid.uuid4())[:8]}"
+
+    kafka_consumer = BasicConsumer(
+        topics=[f"{beamline_acronym}.bluesky.runengine.documents"],
+        bootstrap_servers=kafka_config["bootstrap_servers"],
+        group_id=unique_group_id,
+        consumer_config=kafka_config["runengine_producer_config"],
+        process_message = print_message,
+    )
+
+    try:
+        kafka_consumer.start_polling(work_during_wait=lambda : plt.pause(.1))
+    except KeyboardInterrupt:
+        print('\nExiting Kafka consumer')
+        return()
+
+
+if __name__ == "__main__":
+    import sys
+    print_kafka_messages(sys.argv[1], sys.argv[2])
